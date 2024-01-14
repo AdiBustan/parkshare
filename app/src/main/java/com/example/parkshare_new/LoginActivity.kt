84ff41aeae4436514b612a61b671a7d34aab3adc
barzas <bar1612000@gmail.com>
2024-01-14 22:31:28 +0200

    Add Login page

---
+++ app/src/main/java/com/example/parkshare_new/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.parkshare_new
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+public class LoginActivity : AppCompatActivity(){
+    var emailField: EditText? = null
+    var passwordField: EditText? = null
+    var nextButton: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        emailField = findViewById(R.id.login_email)
+        passwordField = findViewById(R.id.login_password)
+        nextButton = findViewById(R.id.next_button)
+
+//        nextButton?.setOnClickListener {
+//            //todo - get into home page
+//
+//        }
+
+    }
+}
--- app/src/main/java/com/example/parkshare_new/MainActivity.kt
+++ app/src/main/java/com/example/parkshare_new/MainActivity.kt
@@ -12,11 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val signinButton : Button = findViewById(R.id.SigninButton)
+        val loginButton : Button = findViewById(R.id.alreadySignButton)
 
-        signinButton.setOnClickListener(::onSigninBottonClicked) }
+        signinButton.setOnClickListener(::onSigninButtonClicked)
+        loginButton.setOnClickListener(::onLoginPageButtonClicked)
+    }
 
-    fun onSigninBottonClicked(view: View) {
+    fun onSigninButtonClicked(view: View) {
         val intent = Intent(this, SigninActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onLoginPageButtonClicked(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 }
